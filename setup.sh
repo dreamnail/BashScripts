696cc55c1d0ce5be7ed904e426d344364df4c362
dreamnail <123031757+dreamnailx@users.noreply.github.com>
2023-05-28 19:33:44 -0400

    moving files

--- setup.sh
+++ setup.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-function getYesOrNo()
+getYesOrNo()
 {
     local answer
     read -r answer
@@ -11,13 +11,18 @@ function getYesOrNo()
     echo "$answer"
 }
 
-function getGPUDrivers()
+getGPUDrivers()
 {
    echo "installing nvidia drivers for any GPU found in 2014 and later .."
-   sudo dnf update -y # and reboot if you are not on the latest kernel
-   sudo dnf install akmod-nvidia # rhel/centos users can use kmod-nvidia instead
-   sudo dnf install xorg-x11-drv-nvidia-cuda #optional for cuda/nvdec/nvenc support
+   dnf update -y # and reboot if you are not on the latest kernel
+   dnf install akmod-nvidia # rhel/centos users can use kmod-nvidia instead
+   dnf install xorg-x11-drv-nvidia-cuda #optional for cuda/nvdec/nvenc support
 }
+
+if [ "$EUID" -ne 0 ]
+  then echo "Please run as root"
+  exit
+fi
 
 echo "MAKE SURE YOUR SYSTEM IS UPDATED BEFORE RUNNING THIS SCRIPT!"
 echo "You are running $(cat /etc/fedora-release). This script was built for Fedora 37. Would you like to continue? (y/n)"
@@ -32,10 +37,10 @@ fi
 ## Install things that should come pre-installed with Fedora 
 
 echo "Installing RPM Fusion..."
-sudo dnf install -y "https://mirrors.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm https://mirrors.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm"
+dnf install -y "https://mirrors.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm https://mirrors.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm"
 echo "Installing multimedia codecs..."
-sudo dnf groupupdate multimedia -y --setop="install_weak_deps=False" --exclude=PackageKit-gstreamer-plugin
-sudo dnf groupupdate -y sound-and-video
+dnf groupupdate multimedia -y --setop="install_weak_deps=False" --exclude=PackageKit-gstreamer-plugin
+dnf groupupdate -y sound-and-video
 
 ## Install VM software
 
@@ -45,7 +50,7 @@ answer=$(getYesOrNo)
 if [[ $answer == "y" ]]; then
 
    if [[ 8 -ge $(grep -E '^flags.*(vmx|svm)' /proc/cpuinfo | wc -l | awk '{if ($1 < 10) system("echo this message means nothing and i should prob find a better way to do this" )}' | wc -w) ]]; then
-       echo "Install virtualization software.." && sudo dnf group install -y --with-optional virtualization && sudo systemctl start libvirtd && sudo systemctl enable libvirtd || echo "Install failed."
+       echo "Install virtualization software.." && dnf group install -y --with-optional virtualization && systemctl start libvirtd && systemctl enable libvirtd || echo "Install failed."
        echo "Verifying KVM kernel modules. You should see kvm_intel or kvm_amd here.. "
        lsmod | grep kvm
        sleep 5
